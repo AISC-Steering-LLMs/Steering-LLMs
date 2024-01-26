d7bce00f0aa7ed27e9d93de5462919c4d48171da
SkyeNygaard <36653727+SkyeNygaard@users.noreply.github.com>
2024-01-25 22:02:59 -0800

    Visualize (#2)
    * Update Types
    
    * Got T-SNE plot done
    
    * Add prompts
    
    * Sorted data into input and output folders. Within output folder, a new folder with date and time of run logs the experiment parameters as well as store the activation cache, images and some other stuff. This is just a quick test to see if we like this structure. I suggest in the code we maybe use Hydra if we find we have a lot of different experiment input parameters.
    
    * tsne plotted for every layer of GPT2; and a modified version of Eleni's new interactive prompts as in the inputs with some plots showing some sepraration.
    
    * remove extra files
    
    * Cleaned up code
    
    * Git Guide
    
    ---------
    
    Co-authored-by: Skye Nygaard <spacedinosaur42@gmail.com>
    Co-authored-by: matthew <matthew.james.scott.lee@gmail.com>

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Steering-LLMs
-Clone the repo. Then run python -m pip install -e Steering-LLMs from a Python virtual env.
-This project depends on PyTorch, HuggingFace Transformers, and TransformerLens
+
+# In order to run code
+Create Python virtual environment (place where python packages are installed) with ``python3 -m venv ~/camp``. Activate the virtual environment (which may have to be done each time you run code) with ``source ~/camp/bin/activate``. Then clone the repo and run ``python -m pip install -e Steering-LLMs`` from Python virtual env. Run the code with ``python3 Steering-LLMs/steerllm/main.py Steering-LLMs/data/inputs/prompts_good_evil_justice.xlsx``. This will create a folder with the experiment results in ``Steering-LLMs/data/output``.
+
+# GitHub Basic Workflow
+Adding things to the github involves a few steps. ``Monospace sections`` are intended to be run on the commandline/terminal. I am writing this tutorial using terminal commands, but there are GUI options for github as well.
+
+
+1. Install GitHub
+2. Clone the code ``git clone https://github.com/AISC-Steering-LLMs/Steering-LLMs`` (This may require you to configure your account locally. If this is needed, GitHub should link you to a guide to do so) (Need to change to a folder you are okay with before this. To put in the safety-camp folder inside Downloads folder, ``cd ~/Downloads`` ``mkdir safety-camp`` ``cd safety-camp``)
+3. Create a branch to make your change on ``git branch my-branch``(this is stored separately, and can be viewed from the GitHub website. I have configured so that they will be deleted when code is merged)
+4. Change your branch to the newly created one ``git checkout my-branch`` (the default branch is main. This is where your changes will eventually end up. You can go back to main with ``git checkout main``)
+5. Add your file or make your changes within the folder. This may involve adding more prompts, adding notes, adding a run, or changing code
+6. add your changes to git ``git add .`` (You can check that the new file/change has been added with ``git status``)
+7. commit your changes ``git commit -m "Added a new file"`` (-m indicates the message that you attach, so we can know what your goal is with the change) (At this point, your change is fully added to git)
+8. push your changes to github server with ``git push --set-upstream origin my-branch`` (You should be able to view the changes on the GitHub website at this point. There is a dropdown at top left above the files/folders.)
+9. Periodically run ``git pull`` to download the latest changes from the server to your computer (It is best to run this before step 3, before creating a new branch) (run ``git config pull.rebase true`` to make sure this runs properly)
+10. If GitHub says 'X commits behind main' on your branch, reach out to tech team. (You need to checkout your branch and run ``git rebase origin/main`` followed by ``git push --force-with-lease``. This can cause issues if done improperly)
+11. Create a pull request (Go to GitHub website, Pull request page at the top. Click New pull request. Select your branch. Click Create pull request) (After this someone from the tech team can add your change to the main of GitHub)
+12. ``git checkout main`` then ``git pull`` to return to the main branch and download any updates, and be ready for any further changes. 
--- steerllm/main.py
+++ steerllm/main.py
@@ -1,64 +1,224 @@
 # Imports
 import transformer_lens
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
 import os
 import pickle
+import torch
+import sys
+import sklearn
+from sklearn.manifold import TSNE
+from typing import Any
+from dataclasses import dataclass
+import datetime
+import json
 
 # Constants
 PROMPT_COLUMN = "Prompt"
 ETHICAL_AREA_COLUMN = "Ethical_Area"
 POS_COLUMN = "Positive"
-DATA_PATH = os.path.join("..", "data")
-PROMPT_FILE = "prompts.csv"
-OUTPUT_PICKLE = os.path.join(DATA_PATH, "activations_cache.pkl")
+SRC_PATH = os.path.dirname(__file__)
+DATA_PATH = os.path.join(SRC_PATH, "..", "data")
+OUTPUT_PICKLE = os.path.join(DATA_PATH, "outputs", "activations_cache.pkl")
 MODEL_NAME = "gpt2-small"
-PROMPT_FILE_PATH = os.path.join(DATA_PATH, PROMPT_FILE)
+SEED = 42
+
+# May need to install tkinter if not included
+matplotlib.use('TkAgg')
+
+
+# Store all relevant data for a given prompt within this class
+# Can construct progressively, assign the data as it is processed
+@dataclass
+class Activation:
+    prompt: str
+    ethical_area: str
+    positive: bool
+    raw_activations: Any
+    hidden_states: list[np.ndarray]
+
 
 # Load ethical prompts
-def csv_to_dictionary(filename: str) -> dict[str, any]:
-    # with open(filename, 'r') as file:
-    #     reader = csv.DictReader(file)
-    df = pd.read_csv(filename)
-    df = df.dropna()
+# Will work with csv or excel
+def csv_to_dictionary(filename: str) -> dict[str, Any]:
+
+    if filename.endswith(".csv"):
+        df = pd.read_csv(filename)
+    elif filename.endswith(".xlsx"):
+        df = pd.read_excel(filename)
+    else:
+        raise Exception("Expected file ending with .csv or .xlsx")
+    # df = df.dropna()
     data = df.to_dict(orient="list")
 
     return data
 
 
 # Compute forward pass and cache data
-def compute_activations(model: any, prompts: list[str]) -> any:
-    activations_cache = []
-    for prompt in prompts:
+def compute_activations(model: Any, activations_cache: list[Activation]) -> None:
+    for act in activations_cache:
         # Run the model and get logits and activations
-        logits, activations = model.run_with_cache(prompt)
-        activations_cache.append(activations)
-        # print(activations)
-    return activations_cache
-
-
-# Write cache data
-def write_cache(filename: str, cache_data: any) -> None:
-    with open(filename, 'wb') as f:
-        pickle.dump(cache_data, f)
+        logits, raw_activations = model.run_with_cache(act.prompt)
+        act.raw_activations = raw_activations
 
 
 # Load cached data from file
-def load_cache(filename: str) -> any:
+def load_cache(filename: str) -> Any:
     with open(filename, 'rb') as f:
         data = pickle.load(f)
     return data
 
 
+# Add the hidden states (the weights of the model between attention/MLP blocks)
+# Use the last token
+# TODO: I am not sure if the "hidden layers" that pytorch gives for RepE are normalized or unnormalized
+def add_numpy_hidden_states(activations_cache: list[Activation]) -> None:
+    for act in activations_cache:
+        raw_act = act.raw_activations
+        for hook_name in raw_act:
+            # Only get the hidden states, the last activations in the block
+            if "hook_resid_post" in hook_name:
+                # Convert to numpy array
+                # calling .cpu() while using a cpu is a no-op. Needed if run with gpu
+                numpy_activation = raw_act[hook_name].cpu().numpy()
+                # Get only the last token activations using '-1'
+                # print("numpy_activation.shape", numpy_activation.shape)
+                act.hidden_states.append(numpy_activation[0][-1])
+
+
+# Extract the data we are actually using from the dictionary
+def populate_data(prompts_dict: dict[str, Any]) -> list[Activation]:
+    activations_cache = []
+    for prompt, ethical_area, positive in zip(prompts_dict[PROMPT_COLUMN],
+                                              prompts_dict[ETHICAL_AREA_COLUMN],
+                                              prompts_dict[POS_COLUMN]):
+        act = Activation(prompt, ethical_area, bool(positive), None, [])
+        activations_cache.append(act)
+    return activations_cache
+
+
+
+
+# Make tsne plot for hidden state of every layer
+# For now, using the last layer, last token as representative embedding for input
+def tsne_plot(activations_cache: list[Activation], images_dir: str) -> None:
+
+
+    # Using activations_cache[0] is arbitrary as they all have the same number of layers
+    # (12 with GPT-2) with representations
+    for layer in range(len(activations_cache[0].hidden_states)):
+        
+        # print(f"layer {layer}")
+
+        data = np.stack([act.hidden_states[layer] for act in activations_cache])
+        labels = [f"{act.ethical_area} {act.positive}" for act in activations_cache]
+
+        # print("data.shape", data.shape)
+
+        tsne = TSNE(n_components=2, random_state=SEED)
+        embedded_data = tsne.fit_transform(data)
+        
+        df = pd.DataFrame(embedded_data, columns=["X", "Y"])
+        df["Ethical Area"] = labels
+        ax = sns.scatterplot(x='X', y='Y', hue='Ethical Area', data=df)
+        
+        plot_path = os.path.join(images_dir, f"tsne_plot_layer_{layer}.png")
+        plt.savefig(plot_path)
+
+        plt.clf()
+
+
+# Initialize output directory with timestamp
+def create_output_directories() -> tuple[str, str]:
+    current_datetime = datetime.datetime.utcnow().strftime('%Y-%m-%d_%H-%M-%S')
+
+    experiment_base_dir = os.path.join(DATA_PATH, "outputs", current_datetime)
+    images_dir = os.path.join(experiment_base_dir, 'images')
+
+    os.makedirs(experiment_base_dir)
+    os.makedirs(images_dir)
+
+    return experiment_base_dir, images_dir
+
+
+# Write cache data
+def write_activations_cache(activations_cache: Any, experiment_base_dir: str) -> None:
+    filename = os.path.join(experiment_base_dir, "activations_cache.pkl")
+    with open(filename, 'wb') as f:
+        pickle.dump(activations_cache, f)
+
+
+# Write experiment parameters
+# TODO: Replace with hydra
+def write_experiement_parameters(experiment_params: dict, experiment_base_dir: str) -> None:
+    with open(os.path.join(experiment_base_dir, "experiment_parameters.json"), 'w') as f:
+        json.dump(experiment_params, f)
+
+  
+
 if __name__ == "__main__":
-
-    prompts_dict = csv_to_dictionary(PROMPT_FILE_PATH)
-    # print(prompts_dict)
-
-    # Load a model (eg GPT-2 Small)
     model = transformer_lens.HookedTransformer.from_pretrained(MODEL_NAME)
 
+    # Run with:
+    # >>> python3 main.py ../data/inputs/prompts_good_evil_justice.xlsx
+    # Check if a filename is provided as an argument
+    # TODO: Use hydra for command line arguments
+    if len(sys.argv) < 2:
+        print("Please provide a filename as an argument.")
+        sys.exit(1)
+
+    # This dictionary will be used to track the parameters of the experiment.
+    # We can add more to it. This is just a start.
+    # It might be good to use Hydra to manage the parameters in a seperate
+    # config file if we have a lot that changes from run to run. We'd have one
+    # place where we know to go to change things. It also lets us seperate out
+    # CONSTANTS at the top of the file from parameters that change.
+    experiment_params = {
+        'model_name': MODEL_NAME,
+        'experiment_notes': "Testing out the transformer lens library."
+    }
+
+    experiment_base_dir, images_dir = create_output_directories()
+
+    input_path = sys.argv[1]
+    prompts_dict = csv_to_dictionary(input_path)
+
+    # Copy the specific prompts being used from inputs to outputs.
+    # Why? While we are still figuring out which prompt datasets work.
+    # Prompt datasets as inputs which don't work will probably be deleted over time.
+    # Saving them for a specific output lets us track which prompts were used for which output.
+    # We can stop doing this once we have prompts that work.
+    input_file_stem  = os.path.splitext(os.path.basename(input_path))[0]
+    prompts_output_path = os.path.join(experiment_base_dir, f"{input_file_stem}.csv")
+    prompts_df = pd.DataFrame(prompts_dict)
+    prompts_df.to_csv(prompts_output_path)
+
+    activations_cache  = populate_data(prompts_dict)
+
+    # Can use pudb as interactive commandline debugger
     # import pudb; pu.db
-    activations_cache = compute_activations(model, prompts_dict[PROMPT_COLUMN])
     
-    write_cache(OUTPUT_PICKLE, activations_cache)
+    compute_activations(model, activations_cache)
+    
+    add_numpy_hidden_states(activations_cache)
+    
+    tsne_plot(activations_cache, images_dir)
+
+    # Each transformer component has a HookPoint for every activation, which
+    # wraps around that activation.
+    # The HookPoint acts as an identity function, but has a variety of helper
+    # functions that allows us to put PyTorch hooks in to edit and access the
+    # relevant activation
+    # Relevant when changing the model
+    
+    # TODO: Create the raster plot
+    # np.digitize useful for raster plot - https://www.earthdatascience.org/courses/use-data-open-source-python/intro-raster-data-python/raster-data-processing/classify-plot-raster-data-in-python/
+    # TODO: Cluster with PCS in addition to TSNE, figure out which are most simlar
+
+    write_experiement_parameters(experiment_params, experiment_base_dir)
+
+    write_activations_cache(activations_cache, experiment_base_dir)
 
